3ed92ba1526184d6a8eed6c858c5b9982e589994
Jerry Wiltse <jerrywiltse@gmail.com>
2017-09-17 11:32:40 -0400

    Updated version to 1.65.1

--- conanfile.py
+++ conanfile.py
@@ -2,16 +2,16 @@ from conans import ConanFile, tools, os
 
 class BoostIntegerConan(ConanFile):
     name = "Boost.Integer"
-    version = "1.64.0"
+    version = "1.65.1"
     short_paths = True
     url = "https://github.com/bincrafters/conan-boost-integer"
-    description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
+    description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_names = ["integer"]
-    requires =  "Boost.Config/1.64.0@bincrafters/testing", \
-                      "Boost.Assert/1.64.0@bincrafters/testing", \
-                      "Boost.Core/1.64.0@bincrafters/testing", \
-                      "Boost.Static_Assert/1.64.0@bincrafters/testing"
+    requires =  "Boost.Config/1.65.1@bincrafters/testing", \
+                      "Boost.Assert/1.65.1@bincrafters/testing", \
+                      "Boost.Core/1.65.1@bincrafters/testing", \
+                      "Boost.Static_Assert/1.65.1@bincrafters/testing"
                       
     def source(self):
         boostorg_github = "https://github.com/boostorg"
