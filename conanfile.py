9abdf0ade41393175c72e755d758d840d5185f89
Rene Rivera <grafikrobot@gmail.com>
2018-11-29 22:01:12 -0600

    Initial 1.68.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.67.0@bincrafters/testing")
+base = python_requires("boost_base/1.68.0@bincrafters/testing")
 
 class BoostIntegerConan(base.BoostBaseConan):
     name = "boost_integer"
@@ -17,5 +17,3 @@ class BoostIntegerConan(base.BoostBaseConan):
         "boost_core",
         "boost_static_assert"
     ]
-
-
