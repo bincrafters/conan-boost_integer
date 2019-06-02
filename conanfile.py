ee3df8362149f6ff6eaeb40eaf8d601a9d09146f
Rene Rivera <grafikrobot@gmail.com>
2019-06-02 01:13:05 -0500

    Boost 1.70.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,17 +4,9 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/2.0.0@bincrafters/testing")
+
 
 class BoostIntegerConan(base.BoostBaseConan):
     name = "boost_integer"
-    version = "1.69.0"
-    url = "https://github.com/bincrafters/conan-boost_integer"
-    lib_short_names = ["integer"]
-    header_only_libs = ["integer"]
-    b2_requires = [
-        "boost_assert",
-        "boost_config",
-        "boost_core",
-        "boost_static_assert"
-    ]
+    version = "1.70.0"
